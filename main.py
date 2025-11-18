65ff2d2470a3c56bbcbcfe57eba33c7562beaadb
Roberto Hernandez <rhernandez@overridethis.com>
2025-11-18 18:02:58 -0500

    formatter: changed the order of libs import.

--- main.py
+++ main.py
@@ -3,11 +3,12 @@ import os
 import logging
 
 from config import Config
-from stock_ticker import StockTicker
 
 lib_dir = Config.get_epaper_lib_path()
 if os.path.exists(lib_dir):
     sys.path.append(lib_dir)
+
+from stock_ticker import StockTicker
 
 # Configure logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
